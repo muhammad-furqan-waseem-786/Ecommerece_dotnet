de639b98798667487331fceb86a6894d76ba16ec
muhammad-furqan-waseem-786 <muhammadfurqanwaseem786@gmail.com>
2023-04-30 11:57:25 +0500

    Movies controller added

---
+++ Ecommerece_dotnet/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerece_dotnet.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerece_dotnet.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allMovies = _context.Movies.ToListAsync();
+            return View();
+        }
+    }
+}
