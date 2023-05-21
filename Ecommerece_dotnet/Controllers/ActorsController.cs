3f9288782bafb2131aa4ac62d8caf296c81b018e
muhammad-furqan-waseem-786 <muhammadfurqanwaseem786@gmail.com>
2023-05-22 01:04:57 +0500

    Addition of the actors services

--- Ecommerece_dotnet/Controllers/ActorsController.cs
+++ Ecommerece_dotnet/Controllers/ActorsController.cs
@@ -1,20 +1,21 @@
 ﻿using Ecommerece_dotnet.Data;
+using Ecommerece_dotnet.Data.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerece_dotnet.Controllers
 {
     public class ActorsController : Controller
     {
-        private readonly AppDbContext _context;
+        private readonly IActorsService _service;
 
-        public ActorsController(AppDbContext context)
+        public ActorsController(IActorsService service)
         {
-            _context = context;
+            _service = service;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         { 
-            var data = _context.Actors.ToList();
+            var data = await _service.GetAll();
             return View(data);
         }
     }
--- Ecommerece_dotnet/Program.cs
+++ Ecommerece_dotnet/Program.cs
@@ -1,4 +1,5 @@
 using Ecommerece_dotnet.Data;
+using Ecommerece_dotnet.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+builder.Services.AddScoped<IActorsService, ActorsServices>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
