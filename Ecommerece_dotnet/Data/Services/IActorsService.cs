1ca675a64614cb7b0ed0d8a5e5a0cad96b660d04
muhammad-furqan-waseem-786 <muhammadfurqanwaseem786@gmail.com>
2023-05-24 23:06:58 +0500

    Addition of the get all async and get id async in base repository

--- Ecommerece_dotnet/Data/Services/IActorsService.cs
+++ Ecommerece_dotnet/Data/Services/IActorsService.cs
@@ -5,9 +5,13 @@ namespace Ecommerece_dotnet.Data.Services
     public interface IActorsService
     {
         Task<IEnumerable<Actor>> GetAllAsync();
+        
         Task<Actor> GetByIdAsync(int id);
+        
         Task AddAsync(Actor actor);
+        
         Task<Actor> UpdateAsync(int id, Actor newActor);
+        
         Task DeleteAsync(int id);
     }
 }
